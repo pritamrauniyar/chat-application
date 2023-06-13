3fc44af0fcee19eca2ce0c3634dffff158811899
Pritam Rauniyar <pritamrauniyar.np@gmail.com>
2023-06-13 12:40:40 +0545

    Update script.js
    Removed error

--- script.js
+++ script.js
@@ -108,32 +108,12 @@ class Chat {
     });
 
     // Add event listener to send button for sending messages
-    let sendButton = document.querySelector("#chat-send-btn");
-    let messageInput = document.querySelector("#chat-write-msg");
-    sendButton.addEventListener("click", () =>
-      this.sendMessage(messageInput.value)
-    );
+//     let sendButton = document.querySelector("#chat-send-btn");
+//     let messageInput = document.querySelector("#chat-write-msg");
+  
   }
 
-  // Method to send a message
-  sendMessage(message) {
-    if (message.trim() !== "") {
-      // Check if message is not blank
-      // Add new message to this contact's messageList
-      this.data.messageList.push({
-        sender: "USER",
-        message: message,
-        timestamp: new Date().toISOString(),
-      });
-
-      // Clear message input
-      document.querySelector("#chat-write-msg").value = "";
-
-      // Re-render messages
-      this.renderChatMessages();
-    }
-  }
-}
+   
 
 // Class representing the search functionality
 class Search {
